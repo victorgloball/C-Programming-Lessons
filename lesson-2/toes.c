47fd88d3f78022743d691eb842450df0c805a665
VictorGloball <smokinbudda148@gmail.com>
2025-11-13 21:34:31 +0300

    C Primer Plus 2

---
+++ lesson-2/toes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int toes;
+	int doubleToes;
+	int scuareToes;
+
+	toes = 10;
+	doubleToes = toes * 2;
+	scuareToes = toes * toes;
+	printf("Toes = %d\nDoubleToes = %d\nScuareToes = %d",toes,doubleToes,scuareToes);
+	
+	return 0;
+}
